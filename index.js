e9b91361908036a65b9531ab4674d0d2a4bd2161
Happiness Maina <mainahappiness361@gmail.com>
2024-12-04 09:04:34 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+let cats=['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop(-1)
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function removeFirstCat(){
+    cats.shift(0)
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
